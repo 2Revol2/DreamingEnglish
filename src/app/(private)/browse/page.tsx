6177666f6fd2e164605821b20c445369686ffb6b
2Revol2 <maksimbaluta02@gmail.com>
2025-12-03 13:45:07 +0100

    fix: wrap useSearchParams in Suspense to resolve deployment error

--- src/app/(private)/browse/page.tsx
+++ src/app/(private)/browse/page.tsx
@@ -1,11 +1,14 @@
+import { Suspense } from "react";
 import { VideoFilters } from "@/widgets/VideoFilters";
 import { VideosInfiniteList } from "@/features/VideoInfiniteList";
 
 const BrowsePage = async () => {
   return (
     <div className={"flex flex-col gap-4"}>
-      <VideoFilters />
-      <VideosInfiniteList />
+      <Suspense fallback={<div>Loading...</div>}>
+        <VideoFilters />
+        <VideosInfiniteList />
+      </Suspense>
     </div>
   );
 };
