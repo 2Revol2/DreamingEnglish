56630cb5b3fd7216a622a25f3e2f0147802b514b
2Revol2 <maksimbaluta02@gmail.com>
2025-12-10 14:27:52 +0100

    refactor: update video tracking logic

--- src/features/VideoPlayer/ui/VideoPlayer/VideoPlayer.tsx
+++ src/features/VideoPlayer/ui/VideoPlayer/VideoPlayer.tsx
@@ -1,7 +1,8 @@
 "use client";
 import ReactPlayer from "react-player";
-import { useUserData } from "@/entities/User";
-import { useVideoWatchTracker } from "../../lib/useVideoWatchTracker";
+import { useCallback, useEffect, useRef } from "react";
+import { useThrottledCallback } from "use-debounce";
+import { useUpdateUserWatchedTime } from "../../api/useUpdateUserWatchedTime";
 
 interface VideoPlayerProps {
   url: string;
@@ -9,16 +10,45 @@ interface VideoPlayerProps {
 
 export const VideoPlayer = (props: VideoPlayerProps) => {
   const { url } = props;
-  const { data: userId } = useUserData((user) => user.id);
-  const { onPlay, onProgress, onPause, onEnded } = useVideoWatchTracker({ userId });
+
+  const { mutate } = useUpdateUserWatchedTime();
+
+  const playerRef = useRef<HTMLVideoElement | null>(null);
+  const setPlayerRef = useCallback((player: HTMLVideoElement) => {
+    if (!player) return;
+    playerRef.current = player;
+  }, []);
+
+  const watchedSeconds = useRef(0);
+  const lastVideoTime = useRef(0);
+
+  const handleTimeUpdate = useThrottledCallback(() => {
+    const player = playerRef.current;
+    if (!player) return;
+
+    const currentSeconds = Number(player.currentTime.toFixed());
+    const diff = currentSeconds - lastVideoTime.current;
+
+    if (diff > 0 && diff < 5) {
+      watchedSeconds.current += diff;
+    }
+
+    lastVideoTime.current = currentSeconds;
+  }, 500);
+
+  useEffect(() => {
+    return () => {
+      mutate({
+        watchedSeconds: watchedSeconds.current,
+      });
+    };
+  }, [mutate]);
 
   return (
     <div className="relative w-full aspect-video">
       <ReactPlayer
-        onPlay={onPlay}
-        onPause={onPause}
-        onProgress={onProgress}
-        onEnded={onEnded}
+        ref={setPlayerRef}
+        onTimeUpdate={handleTimeUpdate}
         src={url}
         controls
         width="100%"
