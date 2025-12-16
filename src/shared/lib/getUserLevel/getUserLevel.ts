f8eba6f451fb0de13a4829cf7ee3c3ca20b2f659
2Revol2 <maksimbaluta02@gmail.com>
2025-12-16 15:04:45 +0100

    refactor: improve getUserLevel helper

--- src/shared/lib/getUserLevel/getUserLevel.ts
+++ src/shared/lib/getUserLevel/getUserLevel.ts
@@ -1,9 +1,19 @@
+interface LevelConfig {
+  level: number;
+  minSeconds: number;
+  maxSeconds: number;
+}
+
+const LEVELS: LevelConfig[] = [
+  { level: 1, minSeconds: 0, maxSeconds: 180000 },
+  { level: 2, minSeconds: 180000, maxSeconds: 540000 },
+  { level: 3, minSeconds: 540000, maxSeconds: 1080000 },
+  { level: 4, minSeconds: 1080000, maxSeconds: 2160000 },
+  { level: 5, minSeconds: 2160000, maxSeconds: 3600000 },
+  { level: 6, minSeconds: 3600000, maxSeconds: 5400000 },
+  { level: 7, minSeconds: 5400000, maxSeconds: Infinity },
+];
+
 export const getUserLevel = (totalInputSeconds: number) => {
-  if (totalInputSeconds < 180000) return { level: 1, minSeconds: 0, maxSeconds: 180000 };
-  else if (totalInputSeconds < 540000) return { level: 2, minSeconds: 180000, maxSeconds: 540000 };
-  else if (totalInputSeconds < 1080000) return { level: 3, minSeconds: 540000, maxSeconds: 1080000 };
-  else if (totalInputSeconds < 2160000) return { level: 4, minSeconds: 1080000, maxSeconds: 2160000 };
-  else if (totalInputSeconds < 3600000) return { level: 5, minSeconds: 2160000, maxSeconds: 3600000 };
-  else if (totalInputSeconds < 5400000) return { level: 6, minSeconds: 3600000, maxSeconds: 5400000 };
-  else return { level: 7, minSeconds: 5400000, maxSeconds: Infinity };
+  return LEVELS.find((config) => totalInputSeconds < config.maxSeconds) || LEVELS[LEVELS.length - 1];
 };
