540b348211ce805fa08cfb92a555f093af89f3ab
2Revol2 <maksimbaluta02@gmail.com>
2026-01-01 13:16:30 +0100

    chore: add two project to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,8 @@
 import path from "path";
 import { defineConfig } from "vitest/config";
-import { playwright } from "@vitest/browser-playwright";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
+import { playwright } from "@vitest/browser-playwright";
 
 export default defineConfig({
   plugins: [
@@ -19,29 +19,47 @@ export default defineConfig({
     },
   },
   test: {
-    include: ["./**/*.visual.test.tsx", "./**/*.test.tsx", "./**/*.test.ts"],
-    setupFiles: "./test/vitest-setup.ts",
-    browser: {
-      enabled: true,
-      provider: playwright(),
-      instances: [
-        {
-          browser: "chromium",
-          viewport: { width: 1280, height: 720 },
-        },
-      ],
-      expect: {
-        toMatchScreenshot: {
-          comparatorName: "pixelmatch",
-          comparatorOptions: {
-            // 0-1, how different can colors be?
-            threshold: 0.01,
-            // 1% of pixels can differ
-            allowedMismatchedPixelRatio: 0,
+    exclude: ["node_modules/**"],
+    projects: [
+      // ui
+      {
+        extends: true,
+        test: {
+          include: ["**/*.visual.test.tsx"],
+          name: "ui",
+          setupFiles: "./test/vitest-setup.ts",
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            instances: [
+              {
+                browser: "chromium",
+                viewport: { width: 1280, height: 720 },
+              },
+            ],
+            expect: {
+              toMatchScreenshot: {
+                comparatorName: "pixelmatch",
+                comparatorOptions: {
+                  // 0-1, how different can colors be?
+                  threshold: 0,
+                  // 1% of pixels can differ
+                  allowedMismatchedPixelRatio: 0,
+                },
+              },
+            },
+            screenshotDirectory: "./test/__screenshots__",
           },
         },
       },
-      screenshotDirectory: "./test/__screenshots__",
-    },
+      // unit
+      {
+        extends: true,
+        test: {
+          name: "unit",
+          exclude: ["**/*.visual.test.tsx"],
+        },
+      },
+    ],
   },
 });
