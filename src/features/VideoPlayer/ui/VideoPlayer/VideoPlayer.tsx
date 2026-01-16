042b044f9407a0659ea665c10b7b78d580fe4455
2Revol2 <maksimbaluta02@gmail.com>
2026-01-16 09:59:43 +0100

    fix: correct video progress tracking logic

--- src/features/VideoPlayer/ui/VideoPlayer/VideoPlayer.tsx
+++ src/features/VideoPlayer/ui/VideoPlayer/VideoPlayer.tsx
@@ -2,6 +2,7 @@
 import ReactPlayer from "react-player";
 import { useCallback, useEffect, useRef } from "react";
 import { useThrottledCallback } from "use-debounce";
+import { updateUserWatchTime } from "../../api/updateUserWatchTime";
 import { useUpdateUserWatchedTime } from "../../api/useUpdateUserWatchedTime";
 
 interface VideoPlayerProps {
@@ -22,6 +23,9 @@ export const VideoPlayer = (props: VideoPlayerProps) => {
   const watchedSeconds = useRef(0);
   const lastVideoTime = useRef(0);
 
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const isPlayingRef = useRef(false);
+
   const handleTimeUpdate = useThrottledCallback(() => {
     const player = playerRef.current;
     if (!player) return;
@@ -36,11 +40,42 @@ export const VideoPlayer = (props: VideoPlayerProps) => {
     lastVideoTime.current = currentSeconds;
   }, 500);
 
+  // we don`t use tanstack here to prevent component rerender to save video player state
+  const startInterval = () => {
+    if (intervalRef.current) return;
+
+    intervalRef.current = setInterval(async () => {
+      if (watchedSeconds.current > 0) {
+        await updateUserWatchTime({ watchedSeconds: watchedSeconds.current });
+        watchedSeconds.current = 0;
+      }
+    }, 60000);
+  };
+
+  const stopInterval = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+  };
+
+  const handlePlay = () => {
+    isPlayingRef.current = true;
+    startInterval();
+  };
+
+  const handlePause = () => {
+    isPlayingRef.current = false;
+    stopInterval();
+  };
+
+  // here we use tanstack only to update ui after leaving page
   useEffect(() => {
     return () => {
       mutate({
         watchedSeconds: watchedSeconds.current,
       });
+      stopInterval();
     };
   }, [mutate]);
 
@@ -48,6 +83,8 @@ export const VideoPlayer = (props: VideoPlayerProps) => {
     <div className="relative w-full aspect-video">
       <ReactPlayer
         ref={setPlayerRef}
+        onPlay={handlePlay}
+        onPause={handlePause}
         onTimeUpdate={handleTimeUpdate}
         src={url}
         controls
