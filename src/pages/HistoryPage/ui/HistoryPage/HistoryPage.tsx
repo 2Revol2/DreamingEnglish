b8cad62ffb69293e7c87c06c2745020931654a47
2Revol2 <maksimbaluta02@gmail.com>
2026-01-27 11:26:42 +0100

    feat: add breadcrumb to history page

--- src/pages/HistoryPage/ui/HistoryPage/HistoryPage.tsx
+++ src/pages/HistoryPage/ui/HistoryPage/HistoryPage.tsx
@@ -1,13 +1,32 @@
 import { getUserVideosHistory, VideoHistoryList } from "@/entities/Video";
 import { NoResultsIcon } from "@/shared/assets/NoResultsIcon";
 import { Container } from "@/shared/ui/container";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/shared/ui/breadcrumb";
+import { RoutePath } from "@/shared/constants/router";
 
 export const HistoryPage = async () => {
   const historyData = await getUserVideosHistory();
 
   return (
-    <Container className={"lg:pt-8 pt-0"}>
-      <h3 className={"text-3xl font-bold"}>History</h3>
+    <Container className={"lg:pt-8 pt-4"}>
+      <Breadcrumb className={"bg-secondary-background lg:mb-8 mb-4 mx-4 lg:mx-0 rounded-lg p-3"}>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href={RoutePath.library}>Library</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>History</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
       {historyData.length > 0 ? (
         <VideoHistoryList historyList={historyData} />
       ) : (
